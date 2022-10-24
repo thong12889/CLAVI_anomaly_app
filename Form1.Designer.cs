e15c008e930d0d91034fe7d69ce3268d8db02ff0
thong12889 <thongchai_12889@hotmail.com>
2022-10-24 09:27:00 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Anomaly_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.modelBtn = new System.Windows.Forms.Button();
+            this.imageBtn = new System.Windows.Forms.Button();
+            this.modelTbox = new System.Windows.Forms.TextBox();
+            this.imageTbox = new System.Windows.Forms.TextBox();
+            this.modelpathLabel = new System.Windows.Forms.Label();
+            this.imagepathLabel = new System.Windows.Forms.Label();
+            this.predictBtn = new System.Windows.Forms.Button();
+            this.savepathLabel = new System.Windows.Forms.Label();
+            this.savepathTbox = new System.Windows.Forms.TextBox();
+            this.savepathBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.outputTbox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // modelBtn
+            // 
+            this.modelBtn.Location = new System.Drawing.Point(512, 59);
+            this.modelBtn.Name = "modelBtn";
+            this.modelBtn.Size = new System.Drawing.Size(32, 23);
+            this.modelBtn.TabIndex = 1;
+            this.modelBtn.Text = "...";
+            this.modelBtn.UseVisualStyleBackColor = true;
+            this.modelBtn.Click += new System.EventHandler(this.modelBtn_Click);
+            // 
+            // imageBtn
+            // 
+            this.imageBtn.Location = new System.Drawing.Point(512, 115);
+            this.imageBtn.Name = "imageBtn";
+            this.imageBtn.Size = new System.Drawing.Size(32, 23);
+            this.imageBtn.TabIndex = 2;
+            this.imageBtn.Text = "...";
+            this.imageBtn.UseVisualStyleBackColor = true;
+            this.imageBtn.Click += new System.EventHandler(this.imageBtn_Click);
+            // 
+            // modelTbox
+            // 
+            this.modelTbox.Location = new System.Drawing.Point(80, 60);
+            this.modelTbox.Name = "modelTbox";
+            this.modelTbox.ReadOnly = true;
+            this.modelTbox.Size = new System.Drawing.Size(426, 23);
+            this.modelTbox.TabIndex = 3;
+            // 
+            // imageTbox
+            // 
+            this.imageTbox.Location = new System.Drawing.Point(80, 115);
+            this.imageTbox.Name = "imageTbox";
+            this.imageTbox.ReadOnly = true;
+            this.imageTbox.Size = new System.Drawing.Size(426, 23);
+            this.imageTbox.TabIndex = 4;
+            // 
+            // modelpathLabel
+            // 
+            this.modelpathLabel.AutoSize = true;
+            this.modelpathLabel.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.modelpathLabel.Location = new System.Drawing.Point(80, 35);
+            this.modelpathLabel.Name = "modelpathLabel";
+            this.modelpathLabel.Size = new System.Drawing.Size(80, 19);
+            this.modelpathLabel.TabIndex = 5;
+            this.modelpathLabel.Text = "Model Path";
+            // 
+            // imagepathLabel
+            // 
+            this.imagepathLabel.AutoSize = true;
+            this.imagepathLabel.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.imagepathLabel.Location = new System.Drawing.Point(80, 90);
+            this.imagepathLabel.Name = "imagepathLabel";
+            this.imagepathLabel.Size = new System.Drawing.Size(79, 19);
+            this.imagepathLabel.TabIndex = 6;
+            this.imagepathLabel.Text = "Image Path";
+            // 
+            // predictBtn
+            // 
+            this.predictBtn.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.predictBtn.Location = new System.Drawing.Point(568, 59);
+            this.predictBtn.Name = "predictBtn";
+            this.predictBtn.Size = new System.Drawing.Size(96, 133);
+            this.predictBtn.TabIndex = 7;
+            this.predictBtn.Text = "Predict";
+            this.predictBtn.UseVisualStyleBackColor = true;
+            this.predictBtn.Click += new System.EventHandler(this.predictBtn_Click);
+            // 
+            // savepathLabel
+            // 
+            this.savepathLabel.AutoSize = true;
+            this.savepathLabel.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.savepathLabel.Location = new System.Drawing.Point(80, 145);
+            this.savepathLabel.Name = "savepathLabel";
+            this.savepathLabel.Size = new System.Drawing.Size(69, 19);
+            this.savepathLabel.TabIndex = 8;
+            this.savepathLabel.Text = "Save Path";
+            // 
+            // savepathTbox
+            // 
+            this.savepathTbox.Location = new System.Drawing.Point(80, 170);
+            this.savepathTbox.Name = "savepathTbox";
+            this.savepathTbox.ReadOnly = true;
+            this.savepathTbox.Size = new System.Drawing.Size(426, 23);
+            this.savepathTbox.TabIndex = 9;
+            // 
+            // savepathBtn
+            // 
+            this.savepathBtn.Location = new System.Drawing.Point(512, 170);
+            this.savepathBtn.Name = "savepathBtn";
+            this.savepathBtn.Size = new System.Drawing.Size(32, 23);
+            this.savepathBtn.TabIndex = 10;
+            this.savepathBtn.Text = "...";
+            this.savepathBtn.UseVisualStyleBackColor = true;
+            this.savepathBtn.Click += new System.EventHandler(this.savepathBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.progressBar1.Location = new System.Drawing.Point(80, 223);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(584, 10);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // outputTbox
+            // 
+            this.outputTbox.AllowDrop = true;
+            this.outputTbox.Location = new System.Drawing.Point(80, 242);
+            this.outputTbox.Multiline = true;
+            this.outputTbox.Name = "outputTbox";
+            this.outputTbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTbox.Size = new System.Drawing.Size(584, 73);
+            this.outputTbox.TabIndex = 12;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusLabel.Location = new System.Drawing.Point(80, 199);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(47, 19);
+            this.statusLabel.TabIndex = 13;
+            this.statusLabel.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(738, 346);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.outputTbox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.savepathBtn);
+            this.Controls.Add(this.savepathTbox);
+            this.Controls.Add(this.savepathLabel);
+            this.Controls.Add(this.predictBtn);
+            this.Controls.Add(this.imagepathLabel);
+            this.Controls.Add(this.modelpathLabel);
+            this.Controls.Add(this.imageTbox);
+            this.Controls.Add(this.modelTbox);
+            this.Controls.Add(this.imageBtn);
+            this.Controls.Add(this.modelBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CLAVI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button modelBtn;
+        private System.Windows.Forms.Button imageBtn;
+        private System.Windows.Forms.TextBox modelTbox;
+        private System.Windows.Forms.TextBox imageTbox;
+        private System.Windows.Forms.Label modelpathLabel;
+        private System.Windows.Forms.Label imagepathLabel;
+        private System.Windows.Forms.Button predictBtn;
+        private System.Windows.Forms.Label savepathLabel;
+        private System.Windows.Forms.TextBox savepathTbox;
+        private System.Windows.Forms.Button savepathBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox outputTbox;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using OpenCvSharp;
+
+namespace Anomaly_app
+{
+    public partial class Form1 : Form
+    {
+        private InferenceSession sess = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void modelBtn_Click(object sender, EventArgs e)
+        {
+            modelTbox.Text = "";
+
+            OpenFileDialog openFileModel = new OpenFileDialog
+            {
+                Filter = "ONNX files (*.onnx)|*.onnx",
+            };
+
+            if (openFileModel.ShowDialog() == DialogResult.OK)
+            {
+                modelTbox.Text = openFileModel.FileName;
+            }
+        }
+
+        private void imageBtn_Click(object sender, EventArgs e)
+        {
+            imageTbox.Text = "";
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    imageTbox.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void predictBtn_Click(object sender, EventArgs e)
+        {
+            if (modelTbox.Text == "")
+            {
+                MessageBox.Show("Please Insert Model !", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (imageTbox.Text == "")
+                {
+                    MessageBox.Show("Please Insert Image Path !", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if (savepathTbox.Text == "")
+                    {
+                        MessageBox.Show("Please Insert Save Path !", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Init_model(modelTbox.Text);
+                            Process_prediction();
+                        }
+                        catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            sess?.Dispose();
+                            sess = null;
+                        }
+                    }
+                }
+            }
+        }
+        private void Process_prediction()
+        {
+            //double opacity = 0.6;
+            int inputW = 224;
+            int inputH = 224;
+            Size imgSize = new Size(inputW, inputH);
+            var inputMeta = sess.InputMetadata;
+            var inputName = inputMeta.Keys.ToArray()[0];
+
+            string imagePath = imageTbox.Text;
+            string[] filePaths = Directory.GetFiles(imagePath);
+
+            // Display the ProgressBar control.
+            progressBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            progressBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            progressBar1.Maximum = filePaths.Length;
+            // Set the initial value of the ProgressBar.
+            progressBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            progressBar1.Step = 1;
+
+            foreach (var fp in filePaths)
+            {
+                var image = Cv2.ImRead(fp);
+                var data = DataPreprocessing(image);
+                Mat imageFloat = data.Resize(imgSize);
+                var input = new DenseTensor<float>(MatToList(imageFloat), new[] { 1, 3, imgSize.Height, imgSize.Width });
+
+                // Setup inputs and outputs
+                var inputs = new List<NamedOnnxValue>
+                {
+                    NamedOnnxValue.CreateFromTensor(inputName, input)
+                };
+
+                Mat result_anomaly = image.Clone();
+                result_anomaly = result_anomaly.Resize(imgSize);
+                using (var results = sess.Run(inputs))
+                {
+                    var resultsArray = results.ToArray();
+                    var map_value = resultsArray[0].AsEnumerable<float>().ToArray();
+                    var map_dim = resultsArray[0].AsTensor<float>().Dimensions.ToArray();
+                    var score_value = resultsArray[1].AsEnumerable<float>().ToArray();
+                    var score_dim = resultsArray[1].AsTensor<float>().Dimensions.ToArray();
+
+                    var min = map_value.Min();
+                    var max = map_value.Max();
+
+                    /*var normalizeMap = map_value.Select(x => ((x - min) / (max - min)) * 255).ToArray();
+                    var heatMap = GetHeatmap(map_value, map_dim, normalizeMap);
+                    Cv2.ApplyColorMap(heatMap, heatMap, ColormapTypes.Jet);
+                    Cv2.AddWeighted(result_anomaly, opacity, heatMap, 1 - opacity, 0, heatMap);*/
+
+                    var mask = GetResultMask(map_value, map_dim, score_value[0]);
+
+                    var gray = new Mat();
+                    Cv2.CvtColor(mask, gray, ColorConversionCodes.BGR2GRAY);
+                    OpenCvSharp.Point[][] contours;
+                    OpenCvSharp.HierarchyIndex[] hindex;
+                    Cv2.FindContours(gray, out contours, out hindex, RetrievalModes.CComp, ContourApproximationModes.ApproxNone);
+                    Cv2.DrawContours(result_anomaly, contours, -1, new Scalar(0, 0, 255), 1);
+
+                    result_anomaly = result_anomaly.Resize(image.Size());
+                    var filename = Path.GetFileName(fp);
+                    Cv2.ImWrite(savepathTbox.Text + "/" + filename, result_anomaly);
+
+                    progressBar1.PerformStep();
+                    results.Dispose();
+                }
+            }
+            outputTbox.Text += "Results are saved at " + savepathTbox.Text + Environment.NewLine;
+            sess?.Dispose();
+            sess = null;
+        }
+        public static Mat GetHeatmap(float[] output_value, int[] output_dim, float[] HM_value)
+        {
+            Mat mat = new Mat(new Size(output_dim[2], output_dim[3]), MatType.CV_8UC3);
+            for (int batch = 0; batch < output_dim[0]; batch++)
+            {
+                for (int cls = 0; cls < output_dim[1]; cls++)
+                {
+                    for (int h = 0; h < output_dim[2]; h++)
+                    {
+                        for (int w = 0; w < output_dim[3]; w++)
+                        {
+                            int idx = (batch * output_dim[1] * output_dim[2] * output_dim[3]) + (cls * output_dim[2] * output_dim[3]) + (h * output_dim[3]) + w;
+
+                            Vec3b pix = mat.At<Vec3b>(h, w);
+                            pix = new Vec3b((byte)HM_value[idx], (byte)HM_value[idx], (byte)HM_value[idx]);
+                            mat.Set<Vec3b>(h, w, pix);
+                        }
+                    }
+                }
+            }
+            return mat;
+        }
+        public static Mat GetResultMask(float[] output_value, int[] output_dim, float output2_value)
+        {
+            Mat mat = new Mat(new Size(output_dim[2], output_dim[3]), MatType.CV_8UC3);
+            for (int batch = 0; batch < output_dim[0]; batch++)
+            {
+                for (int cls = 0; cls < output_dim[1]; cls++)
+                {
+                    for (int h = 0; h < output_dim[2]; h++)
+                    {
+                        for (int w = 0; w < output_dim[3]; w++)
+                        {
+                            int idx = (batch * output_dim[1] * output_dim[2] * output_dim[3]) + (cls * output_dim[2] * output_dim[3]) + (h * output_dim[3]) + w;
+
+                            Vec3b pix = mat.At<Vec3b>(h, w);
+                            if (output_value[idx] < output2_value)
+                            {
+                                pix = new Vec3b(0, 0, 0);
+                            }
+                            else
+                            {
+                                pix = new Vec3b(255, 255, 255);
+                            }
+                            mat.Set<Vec3b>(h, w, pix);
+                        }
+                    }
+                }
+            }
+            return mat;
+        }
+        private void Init_model(string modelPath)
+        {
+            try
+            {
+                var option = new SessionOptions();
+                //var option = SessionOptions.MakeSessionOptionWithCudaProvider(0);
+                option.GraphOptimizationLevel = GraphOptimizationLevel.ORT_DISABLE_ALL;
+                sess = new InferenceSession(modelPath, option);
+
+                outputTbox.Text += "Initial model successful !" + Environment.NewLine;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Mat DataPreprocessing(Mat image)
+        {
+            Mat data = Mat.Zeros(image.Size(), MatType.CV_32FC3);
+            using (var rgbImage = new Mat())
+            {
+                Cv2.CvtColor(image, rgbImage, ColorConversionCodes.BGR2RGB);
+                rgbImage.ConvertTo(data, MatType.CV_32FC3, (float)(1 / 255.0));
+                var channelData = Cv2.Split(data);
+                channelData[0] = (channelData[0] - 0.485) / 0.229;
+                channelData[1] = (channelData[1] - 0.456) / 0.224;
+                channelData[2] = (channelData[2] - 0.406) / 0.225;
+                Cv2.Merge(channelData, data);
+            }
+            return data;
+        }
+
+        private static float[] MatToList(Mat mat)
+        {
+            var ih = mat.Height;
+            var iw = mat.Width;
+            var chn = mat.Channels();
+            unsafe
+            {
+                return Create((float*)mat.DataPointer, ih, iw, chn);
+            }
+        }
+        private unsafe static float[] Create(float* ptr, int ih, int iw, int chn)
+        {
+            float[] array = new float[chn * ih * iw];
+
+            for (int y = 0; y < ih; y++)
+            {
+                for (int x = 0; x < iw; x++)
+                {
+                    for (int c = 0; c < chn; c++)
+                    {
+                        var idx = (y * chn) * iw + (x * chn) + c;
+                        var idx2 = (c * iw) * ih + (y * iw) + x;
+                        array[idx2] = ptr[idx];
+                    }
+                }
+            }
+            return array;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sess?.Dispose();
+            sess = null;
+        }
+
+        private void savepathBtn_Click(object sender, EventArgs e)
+        {
+            savepathTbox.Text = "";
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    savepathTbox.Text = fbd.SelectedPath;
+                }
+            }
+        }
+    }
+}
